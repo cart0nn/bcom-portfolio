f60639b2cfbec694b97d9cc4f76d842da2cfd7fe
Carson Smith <smittycart127@outlook.com>
2024-12-13 11:24:53 -0600

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+content_path="$PORTFOLIO_ROOT/dist"
+
+if test -d content_path; then
+  echo 'Webserver content already exists, removing.'
+  rm -r content_path
+fi
+
+if test -d dist; then
+  echo 'Old static files present, removing.'
+  rm -r dist
+fi
+
+git pull
+
+npm run build
+
+echo "Copying static files to server root."
+cp -r dist content_path
+
+echo "Restarting nginx."
+systemctl restart nginx
